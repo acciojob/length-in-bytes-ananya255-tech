5637a06509c3b2606327351b9085e6c607a452be
Ananya Virwani <ananyavirwani255@gmail.com>
2026-09-08 14:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	byteSize(str)
+	if(byteSize(str)==''){
+		return byteSize(0)
+	}
 };
-
+return byteSize
 // Do not change the code below
+
 const str = prompt("Enter some string.");
 alert(byteSize(str));
